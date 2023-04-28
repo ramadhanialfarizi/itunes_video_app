25a972a3622e4e61e977d200d5d982b87687b3cc
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2023-04-28 18:35:30 +0700

    turn off rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:itunes_video_app/core/utils/routes.dart';
 import 'package:itunes_video_app/features/authentication/view_model/signin_provider.dart';
 import 'package:itunes_video_app/features/authentication/view_model/signup_provider.dart';
@@ -33,6 +34,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
